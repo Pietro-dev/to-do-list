b3ad8da38a42a81928ba4f483255e8f8879e6069
Pietro Martins <pietro.pietromartins@gmail.com>
2024-05-24 15:43:02 -0300

    atualização dos pacotes

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+// importação do express
 const express = require('express')
-const checkListRouter = require('./src/routes/checklist')
-require('./config/database')
+// const checkListRouter = require('./src/routes/checklist')
+// require('./config/database')
 
+// constante para usar todos os métodos do express
 const app = express()
-app.use(express.json())
 
-app.use('/checklists', checkListRouter)
+// criando a rota de início do app
+app.get('/', (req, res) => {
+    res.send('<h1>Minha lista de tarefas</h1>')
+})
 
+app.get('/json', (req, res) => {
+    res.json({title: 'Tarefa x', done: true})
+})
+
+// inicialização do servidor
 app.listen(3000, () => {
     console.log('Servidor foi iniciado')
 })
+
+// app.use(express.json())
+
+// app.use('/checklists', checkListRouter)
